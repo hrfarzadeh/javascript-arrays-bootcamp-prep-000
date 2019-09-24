ce8961467572eb4209f690d218081b41665d27b0
Hamidreza Fakharzadeh <hrfarzadeh@gmail.com>
2019-09-24 16:59:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,8 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice();
-  return "(${array} + ${array.slice()})";
+  return array.slice();
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
