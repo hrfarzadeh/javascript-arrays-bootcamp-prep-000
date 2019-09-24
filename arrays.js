3f042c68f923be20e44d54eacc0acd7ae08808d8
Hamidreza Fakharzadeh <hrfarzadeh@gmail.com>
2019-09-24 17:14:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,6 +39,5 @@ function destructivelyRemoveElementFromEndOfArray(array){
    
 }
 function removeElementFromEndOfArray(array){
-  var array=[1,2,3]
-  return array.pop();
+   return array.slice(0, array.length-1);
 }
