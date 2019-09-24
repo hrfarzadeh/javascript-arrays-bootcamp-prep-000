c645dd58c909603fb8808b57622518d0309edc84
Hamidreza Fakharzadeh <hrfarzadeh@gmail.com>
2019-09-24 16:16:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@
 
 
 function addElementToBeginningOfArray (array, element){
- return array.unshift(element);
+ return [element,...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
